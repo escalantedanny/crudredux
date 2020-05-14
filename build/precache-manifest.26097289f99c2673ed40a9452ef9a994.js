32b19510079d4a738ecb45aff580936833d2adbc
Danny Escalante <danny.ezequiel@gmail.com>
2020-05-14 09:05:59 -0400

    build
    build

---
+++ build/precache-manifest.26097289f99c2673ed40a9452ef9a994.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2d72318f5e4871d3b03386d312e5b741",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4edc002e3763b1dec4e9",
+    "url": "/static/css/main.dc977176.chunk.css"
+  },
+  {
+    "revision": "e071449e22d7c4c0ddad",
+    "url": "/static/js/2.ec963b73.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.ec963b73.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4edc002e3763b1dec4e9",
+    "url": "/static/js/main.e5861c7d.chunk.js"
+  },
+  {
+    "revision": "23fa50835a8f9a32a7c3",
+    "url": "/static/js/runtime-main.aa1748c1.js"
+  }
+]);
